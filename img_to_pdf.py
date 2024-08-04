218f687ea4a7e6ff57574f3984c8abf0c7e88093
SharvinDmello <sharvin.dmello21@gmail.com>
2024-08-04 15:21:52 +0530

    Update img_to_pdf.py

--- img_to_pdf.py
+++ img_to_pdf.py
@@ -5,20 +5,25 @@ import os
 
 def images_to_pdf(folder_path, pdf_path, pdf_name):
     files = os.listdir(folder_path)
+
     # Filter only image files .jpg and .png
     image_files = [f for f in files if f.endswith(('.jpg', '.png'))]
+
     # Create a new PDF file
     pdf_file_path = os.path.join(pdf_path, f"{pdf_name}.pdf")
     c = canvas.Canvas(pdf_file_path, pagesize=letter)
+
     # Add each image to the PDF
     for image_name in image_files:
         image_path = os.path.join(folder_path, image_name)
         img = Image.open(image_path)
         img_width, img_height = img.size
         aspect_ratio = img_height / img_width
+
         # Set the image size to fit the page width
-        width = letter[0] - 40  # 40 units margin
+        width = letter[0] - 40 #margin
         height = width * aspect_ratio
+
         # Add the image to the PDF
         c.drawImage(image_path, 20, 20, width, height)
         c.showPage()
